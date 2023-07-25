1754c87337a2395da550ee95a9bc157dc55bba84
Sergio Cortes Satizabal <sergiocortessat@gmail.com>
2023-07-25 11:07:28 +0200

    fix app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import "./App.css";
 import PageEditor from './components/PageEditor';
-// wrap the componet with the redux store
-import { Provider } from "react-redux";
-import store from "./store/store";
-
 
 const App: React.FC = () => {
   return (
-    <Provider store={store}>
+    <div className="app">
       <PageEditor />
-    </Provider>
+    </div>
   );
 };
 
